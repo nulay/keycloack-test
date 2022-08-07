2a3cdf1768cf32fa3ca3c062cea9ff4db9dbbe09
nulay <nulay@mail.ru>
2022-08-07 15:21:59 +0300

    change wait time

--- spring-boot-with-keycloak/src/main/java/ru/lanit/service/KeycloakWebService.java
+++ spring-boot-with-keycloak/src/main/java/ru/lanit/service/KeycloakWebService.java
@@ -1,6 +1,12 @@
 package ru.lanit.service;
 
 import lombok.SneakyThrows;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.message.BasicHeader;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
@@ -10,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.lanit.configuration.KeycloakProperties;
 
-import java.net.URLEncoder;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,7 +43,7 @@ public class KeycloakWebService {
 
     @SneakyThrows
     private Connection.Response executeLogin(Map<String, String> cookies, String url) {
-        Map headers = prepaireHeaderToLoginFormRequest();
+        Map<String, String> headers = prepaireHeaderToLoginFormRequest();
 //        URI aURL = new URI(url);
 //        String urlLoginForm = aURL.getScheme() + "://" + aURL.getHost() +
 //                ((aURL.getPort() != 80) ? ":" + aURL.getPort() : "") +
@@ -64,6 +70,28 @@ public class KeycloakWebService {
             log.error("Connection refused to: {}", url, exception);
             log.debug("Check keycloak call {}", loginForm.statusCode());
             log.debug("Body is {}", loginForm.body());
+
+
+            try {
+                DefaultHttpClient httpclient = new DefaultHttpClient();
+                MyRedirectHandler handler = new MyRedirectHandler();
+                httpclient.setRedirectStrategy(handler);
+                CookieStore cookieStore = new BasicCookieStore();
+                cookies.forEach((key, value) -> cookieStore.addCookie(new BasicClientCookie(key, value)));
+                httpclient.setCookieStore(cookieStore);
+
+                HttpPost post = new HttpPost(url);
+                headers.forEach((key, value) -> post.addHeader(new BasicHeader(key, value)));
+                httpclient.execute(post);
+
+                String lastUrl = url;
+                if (handler.lastRedirectedUri != null) {
+                    lastUrl = handler.lastRedirectedUri.toString();
+                }
+                log.debug("lastUrl {}", lastUrl);
+            } catch (IOException e) {
+                log.error("2 error ", exception);
+            }
         }
         log.debug("Size of cookies {}", loginForm.cookies().size());
         return loginForm;
@@ -109,7 +137,7 @@ public class KeycloakWebService {
     }
 
     private static void logDebugCookies(Map<String, String> cookies) {
-        if(log.isDebugEnabled()) {
+        if (log.isDebugEnabled()) {
             final String[] cookiesStr = {""};
             cookies.forEach((key, value) -> {
                 cookiesStr[0] += key + "=" + value + ",";
