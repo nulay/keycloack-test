accb8ce9da7e90d73cbe3246c197961f79a5d829
nulay <nulay@mail.ru>
2022-12-26 12:12:10 +0300

    add new vers

---
+++ spring-boot-with-keycloak/src/main/java/ru/lanit/configuration/SslFix2.java
@@ -0,0 +1,39 @@
+package ru.lanit.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+@Slf4j
+public class SslFix2 {
+    public static void execute() {
+        // WARNING: do it only if security isn't important, otherwise you have
+        // to follow this advices: http://stackoverflow.com/a/7745706/1363265
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            log.info("Error ssl :", e);
+        }
+    }
+}
